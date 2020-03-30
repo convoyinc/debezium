70e498b818727d016c9e40c73a62dd8299894314
Michael Marvick <michael.marvick@convoy.com>
2020-03-30 10:59:55 -0700

    Keep track of processed and unprocessed LSNs, and find the largest processed with no unprocessed before it

--- debezium-connector-postgres/src/main/java/io/debezium/connector/postgresql/PostgresPendingLsnStore.java
+++ debezium-connector-postgres/src/main/java/io/debezium/connector/postgresql/PostgresPendingLsnStore.java
@@ -10,16 +10,16 @@ import java.io.StringWriter;
 import java.util.Collection;
 import java.util.Enumeration;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.atomic.AtomicLong;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 import org.apache.kafka.connect.source.SourceRecord;
 import org.postgresql.replication.LogSequenceNumber;
 
 public class PostgresPendingLsnStore {
-    private final long LARGEST_PROCESSED_LSN_UNSET = -1;
+    private static final String LSN_PROCESSED = "LSN_PROCESSED";
 
     private final ConcurrentHashMap<Long, Integer> lsnsInProgress = new ConcurrentHashMap<>();
-    private final AtomicLong largestProcessedLsn = new AtomicLong(LARGEST_PROCESSED_LSN_UNSET);
+    private final ConcurrentHashMap<Long, String> lsnsProcessed = new ConcurrentHashMap<>();
 
     public void recordPolledLsn(SourceRecord record) {
         Long lsn = getLsnFromRecord(record);
@@ -46,24 +46,39 @@ public class PostgresPendingLsnStore {
             return;
         }
 
+        AtomicBoolean isFullyProcessed = new AtomicBoolean(false);
         lsnsInProgress.compute(lsn, (_lsn, count) -> {
             if (count == null || count < 0) {
                 throw new IllegalStateException("Attempted to record processed LSN when it hasn't been polled");
             } else if (count == 1) {
+                isFullyProcessed.set(true);
                 return null;
             } else {
                 return count - 1;
             }
         });
 
-        largestProcessedLsn.updateAndGet(currentLargestLsn -> Math.max(currentLargestLsn, lsn));
+        if (isFullyProcessed.get()) {
+            lsnsProcessed.put(lsn, LSN_PROCESSED);
+        }
+    }
+
+    /**
+     * Get the largest LSN from an event comitted to Kafka so far.
+     * @return the Long lsn value, or null if there are no events that have been processed yet.
+     */
+    public Long getLargestProcessedLsn() {
+        Long earliestUnprocessedLsn = getEarliestUnprocessedLsn();
+        Long largestProcessedLsn = getLargestProcessedLsnLessThan(earliestUnprocessedLsn);
+        removeProcessedLsnsLessThan(largestProcessedLsn);
+        return largestProcessedLsn;
     }
 
     /**
      * Get the earliest polled LSN from an event that has yet to be committed to Kafka.
      * @return the Long lsn value, or null if there are no unprocessed LSNs
      */
-    public Long getEarliestUnprocessedLsn() {
+    private Long getEarliestUnprocessedLsn() {
         // If there are no elements, return null
         Enumeration<Long> lsnIterator = lsnsInProgress.keys();
         if (!lsnIterator.hasMoreElements()) {
@@ -83,25 +98,62 @@ public class PostgresPendingLsnStore {
     }
 
     /**
-     * Get the largest LSN from an event comitted to Kafka so far.
-     * @return the Long lsn value, or null if there are no events that have been processed yet.
+     * Get the earliest polled LSN from an event that has yet to be committed to Kafka.
+     * @param upperBound
+     * @return the Long lsn value, or null if there are no unprocessed LSNs
      */
-    public Long getLargestProcessedLsn() {
-        Long value = largestProcessedLsn.get();
-        if (value == LARGEST_PROCESSED_LSN_UNSET) {
+    private Long getLargestProcessedLsnLessThan(Long upperBound) {
+        // If there was no upper bound passed in, then set it to Long.MAX_VALUE
+        // so that we get the largest processed lsn so far
+        if (upperBound == null) {
+            upperBound = Long.MAX_VALUE;
+        }
+
+        // Define a const for no matching LSN found, and initialize the largest
+        // lsn to that constant
+        final Long NO_MATCH = -1L;
+        Long largestLsnInBounds = NO_MATCH;
+
+        // Iterate through the processed LSNs to find the largest one that's
+        // less than the upper bound
+        Enumeration<Long> lsnsProcessedIterator = lsnsProcessed.keys();
+        while (lsnsProcessedIterator.hasMoreElements()) {
+            Long lsn = lsnsProcessedIterator.nextElement();
+            if (lsn > largestLsnInBounds && lsn < upperBound) {
+                largestLsnInBounds = lsn;
+            }
+        }
+
+        // Return the largest matching LSN, or null if none was found
+        if (largestLsnInBounds == NO_MATCH) {
             return null;
         } else {
-            return value;
+            return largestLsnInBounds;
         }
     }
 
+    /**
+     * 
+     */
+    private void removeProcessedLsnsLessThan(Long upperBound) {
+        if (upperBound == null) {
+            return;
+        }
+
+        lsnsProcessed.keySet().removeIf((lsn) -> lsn < upperBound);
+    }
+
     public String toString() {
-        StringWriter sw = new StringWriter(50 + lsnsInProgress.size() * 22);
-        sw.append("PostgresPendingLsnStore (");
+        StringWriter sw = new StringWriter(92 + lsnsInProgress.size() * 24 + lsnsProcessed.size() * 20);
+        sw.append("PostgresPendingLsnStore [lsnsInProgress (");
         sw.append(Integer.toString(lsnsInProgress.size()));
         sw.append(" items) [");
         lsnsInProgress.forEach((lsn, count) -> sw.append(LogSequenceNumber.valueOf(lsn) + "=" + count + "; "));
-        sw.append("]");
+        sw.append("]; lsnsProcessed (");
+        sw.append(Integer.toString(lsnsProcessed.size()));
+        sw.append(" items) [");
+        lsnsProcessed.forEach((lsn, count) -> sw.append(LogSequenceNumber.valueOf(lsn) + "; "));
+        sw.append("]]");
         return sw.toString();
     }
 
