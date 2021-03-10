fc980fee35874c30d90808d6f6d5db554a67ce6d
Adrian Kreuziger <akreuziger@convoy.com>
2021-03-10 14:37:22 -0800

    stash

--- debezium-connector-postgres/src/test/java/io/debezium/connector/postgresql/OutboxEventRouterIT.java
+++ debezium-connector-postgres/src/test/java/io/debezium/connector/postgresql/OutboxEventRouterIT.java
@@ -13,6 +13,7 @@ import static org.fest.assertions.Assertions.assertThat;
 
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
